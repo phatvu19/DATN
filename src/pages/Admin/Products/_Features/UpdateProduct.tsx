dae4a7141829047939df4cecb7c47c5efaff6d2a
Vu Duc Phat <you@example.com>
2024-09-17 19:03:27 +0700

    body

--- src/pages/Admin/Products/_Features/UpdateProduct.tsx
+++ src/pages/Admin/Products/_Features/UpdateProduct.tsx
@@ -30,7 +30,6 @@ const UpdateProduct = () => {
     const navigate = useNavigate()
     const { id }: any = useParams()
 
-
     const {
         control,
         handleSubmit,
@@ -77,11 +76,11 @@ const UpdateProduct = () => {
                     ])
                 }
                 if (product.variants.length > 0) {
-                    let arr :any = []
+                    const arr: any = []
                     const attributeNames = product.variants.reduce(
                         (acc: string[], variant: any) => {
                             variant.attribute_values.forEach((attrValue: any) => {
-                                console.log(attrValue);
+                                console.log(attrValue)
                                 arr.push(attrValue?.pivot?.attribute_value_id)
                                 const attributeName =
                                     attrValue.attribute.name.toLowerCase()
@@ -93,7 +92,7 @@ const UpdateProduct = () => {
                         },
                         [],
                     )
-                    console.log(arr);
+                    console.log(arr)
 
                     const formattedVariants = product.variants.map(
                         (variant: any) => {
@@ -172,7 +171,7 @@ const UpdateProduct = () => {
             variants: variants.map((variant: any) => {
                 // Lấy các key từ variant.attributes
                 const attributeKeys = Object.keys(variant.attributes)
-                console.log(variant);
+                console.log(variant)
 
                 return {
                     variant_id: variant.id,
@@ -181,7 +180,9 @@ const UpdateProduct = () => {
                     quantity: variant.quantity,
                     attributes: attributeKeys.map((key, index) => ({
                         atribute_value_id: variant?.attribute_values[index]?.id,
-                        atribute_value_id_old:variant?.attribute_values[index]?.pivot?.attribute_value_id,
+                        atribute_value_id_old:
+                            variant?.attribute_values[index]?.pivot
+                                ?.attribute_value_id,
                         name: key,
                         value: variant.attributes[key],
                     })),
@@ -231,7 +232,7 @@ const UpdateProduct = () => {
         newVariants[index].attributes[attributeType] = value
         setVariants(newVariants)
     }
-    console.log(variants);
+    console.log(variants)
 
     const [check, setcheck] = useState<any>(false)
     const HandleExit = () => {
